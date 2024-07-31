d75633e0c4d0063552ee88b71de08718dfc29a20
phoenix-rohit <rohitrane88888@gmail.com>
2024-07-31 20:42:30 +0530

    Added State and Functionality (Completed)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,68 @@
-function App() {
-  return <div className="App"></div>;
+import { useState } from "react";
+import "./index";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FlashCards />
+    </div>
+  );
 }
 
-export default App;
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+  },
+];
+
+function FlashCards() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+
+  return (
+    <div className="flashcards">
+      {questions.map((item, idx, arr) => {
+        return (
+          <div
+            key={item.id}
+            className={item.id === selectedId ? "selected" : ""}
+            onClick={() => handleClick(item.id)}
+          >
+            <p>{item.id === selectedId ? item.answer : item.question}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
